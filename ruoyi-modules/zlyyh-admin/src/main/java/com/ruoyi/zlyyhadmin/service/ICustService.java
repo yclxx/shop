a1c4cccc0e19d690fa8284659b18c6816ff465be
yclxx <2548762078@qq.com>
2023-09-13 17:24:34 +0800

    剔除无用代码

--- ruoyi-modules/zlyyh-admin/src/main/java/com/ruoyi/zlyyhadmin/service/ICustService.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.zlyyhadmin.service;
-
-import com.ruoyi.zlyyh.domain.Cust;
-import java.util.List;
-
-public interface ICustService {
-
-    List<Cust> selectAll(String startTime,String endTime,int pageNum,int pageSize);
-}
