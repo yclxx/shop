365496a124c287572312f0ae5d76d4cea9e4657f
yclxx <2548762078@qq.com>
2023-09-19 11:57:55 +0800

    新增字段解释功能

---
+++ ruoyi-ui/src/components/DictTooltip/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-tooltip placement="top">
+    <div slot="content">
+      <div v-for="(item, index) in options">
+        <span>{{item.raw.dictLabel}}</span>
+        <span>：</span>
+        <span>{{item.raw.remark}}</span>
+      </div>
+    </div>
+    <i class="el-icon-question"></i>
+  </el-tooltip>
+</template>
+
+<script>
+  export default {
+    name: "DictTooltip",
+    props: {
+      options: {
+        type: Array,
+        default: null,
+      },
+    },
+  };
+</script>
+<style scoped>
+</style>
