d280bfba4e9792bb2ad293a1c653eef10b443329
yclxx <2548762078@qq.com>
2024-03-14 16:29:40 +0800

    商户门店查询

--- ruoyi-common/ruoyi-common-zlyyh/src/main/java/com/ruoyi/zlyyh/domain/bo/QueryShopProductBo.java
+++ ruoyi-common/ruoyi-common-zlyyh/src/main/java/com/ruoyi/zlyyh/domain/bo/QueryShopProductBo.java
@@ -3,7 +3,6 @@ package com.ruoyi.zlyyh.domain.bo;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 /**
  * @author 25487
@@ -21,7 +20,7 @@ public class QueryShopProductBo {
     /** 渠道 */
     private String supportChannel;
     /** 产品类别 */
-    private List<Long> categoryIds;
+    private Long categoryId;
     /** 排序方式 0-推荐排序 1-距离最近 */
     private String orderByType;
 }
